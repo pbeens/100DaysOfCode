1de6fdd4e27f81e5e00f501e87a68fafa31c61ef
Peter Beens <pbeens@gmail.com>
2019-10-16 12:03:03 -0400

    Completed Hackerrank Sets 08

--- Hackerrank/04-Sets/08-Set-.symmetric_difference()-Operation.py
+++ Hackerrank/04-Sets/08-Set-.symmetric_difference()-Operation.py
@@ -1,3 +1,17 @@
 # 08-Set-.symmetric_difference()-Operation.py
 # https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem
 
+sample_input = '''9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+'''  # should return 8
+
+# input
+n1 = int(input())
+students1 = set(input().split())
+n2 = int(input())
+students2 = set(input().split())
+
+# process and output
+print(len(students1 ^ students2))  # aka symmetric_difference()
