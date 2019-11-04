dd28598e15de396ba70f6f38a1756fa4608cdfcc
Peter Beens <pbeens@gmail.com>
2019-11-04 09:44:23 -0500

    Helped a Twitter user with a Python problem
    https://twitter.com/pbeens/status/1191363384999997442

---
+++ Misc-Challenges/Other/help-user.py
@@ -0,0 +1,9 @@
+# in response to https://twitter.com/adlab2019/status/1191179949090230274
+
+name = ''
+
+while len(name) < 1:
+    name = input('What is your name: ')
+
+print('Your name is', name)
+
